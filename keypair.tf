c2990934d94a28b581c3f5d86cf33b892f5ce5c1
RaytraceSG <azmi.maniku@gmail.com>
2024-07-28 22:58:31 +0800

    Update keypair.tf

--- keypair.tf
+++ keypair.tf
@@ -5,11 +5,11 @@ resource "tls_private_key" "azmi1-tf-tls" {
 }
 
 resource "aws_key_pair" "azmi1-tf-keypair" {
-  key_name   = "azmi1-keypair"
+  key_name   = "azmi1-tf-keypair"
   public_key = tls_private_key.azmi1-tf-tls.public_key_openssh
 }
 
-resource "local_file" "azmi1-private-key" {
+resource "local_file" "azmi1-tf-private-key" {
   content  = tls_private_key.azmi1-tf-tls.private_key_pem
   filename = "${path.module}/azmi1-tf-private-key.pem"
   file_permission = "0400"
